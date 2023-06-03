7330c88607f822991d64c3a2af779f2f24f2cdbf
manuela <82252712+manuellla@users.noreply.github.com>
2023-06-03 00:38:27 -0300

    Update Video.cpp

--- Video.cpp
+++ Video.cpp
@@ -5,8 +5,8 @@
 
 using namespace std;
 
-Video::Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep) :
-    _nome(nome), _ano(ano), _genero(genero), _subtipo(subtipo), _filme_duracao(filme_duracao), _n_temp(n_temp), _total_ep(total_ep) {
+Video::Video(string nome, int ano, string genero) :
+    _nome(nome), _ano(ano), _genero(genero) {
 }
 
 float Video::get_media_avaliacao() {
@@ -20,19 +20,4 @@ float Video::get_media_avaliacao() {
 
 void Video::avaliar(int nota) {
     avaliacoes.push_back(nota);
-}
-
-void Video::print_info(int id) {
-    if (_subtipo == "Filme")
-    {
-        cout << "Filme " << id << ": " << _nome << " (" << _ano << "), " << _genero << ", " << _filme_duracao << " min, ";
-        cout << "nota: " << get_media_avaliacao() << std::endl;
-    }
-
-    else if (_subtipo == "Serie")
-    {
-        cout << "Serie " << id << ": " << _nome << " (" << _ano << "), " << _genero << ", ";
-        cout << _n_temp << " temporadas, " << _total_ep << " episodios, ";
-        cout << "nota: " << get_media_avaliacao() << std::endl;
-    }
 }
