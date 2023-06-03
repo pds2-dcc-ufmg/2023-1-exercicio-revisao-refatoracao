6a8763e4272e324e46689e3bae2f0001d48aef05
bernarraxca <116650857+beraldo14@users.noreply.github.com>
2023-06-03 12:21:46 -0300

    Update Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -5,50 +5,45 @@
 #include <iostream>
 #include <list>
 
-using namespace std;
-
 class Video {
 public:
-int ano, filme_duracao, n_temp, total_ep;
-string nome, genero, subtipo;
-list<int> avaliacoes;
+    int ano, filme_duracao, n_temp, total_ep;
+    std::string nome, genero, subtipo;
+    std::list<int> avaliacoes;
 
-Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep) : nome(nome), ano(ano), genero(genero), subtipo(subtipo), filme_duracao(filme_duracao),n_temp(n_temp), total_ep(total_ep)
-{
-}
+    Video(std::string nome, int ano, std::string genero, std::string subtipo, int filme_duracao, int n_temp, int total_ep)
+        : nome(nome), ano(ano), genero(genero), subtipo(subtipo), filme_duracao(filme_duracao), n_temp(n_temp), total_ep(total_ep) {
+    }
 
-float get_media_avaliacao() {
-    float v = 0;
-    for (auto n : avaliacoes)v += n;
-    return v/avaliacoes.size();
-  }
+    float get_media_avaliacao() const {
+        if (!avaliacoes.empty()) {
+            float v = 0;
+            for (auto nota : avaliacoes) {
+                v += nota;
+            }
+            return v / avaliacoes.size();
+        }
+        return 0.0f;
+    }
 
-void avaliar(int nota) {
-  avaliacoes.push_back(nota);
-  }
+    void avaliar(int nota) {
+        avaliacoes.push_back(nota);
+    }
 
-void print_info(int id){
-  if (subtipo == "Filme") 
-  {
-    cout << "Filme " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero;
-    cout << ", " << filme_duracao << " min, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-
-  if (subtipo == "Serie")
-  {
-    cout << "Serie " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero << ", ";
-    cout << n_temp << " temporadas, ";
-    cout << total_ep << " episodios, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-};
+    void print_info(int id) const {
+        if (subtipo == "Filme") {
+            std::cout << "Filme " << id << ": ";
+            std::cout << nome << " (" << ano << "), ";
+            std::cout << genero << ", " << filme_duracao << " min, ";
+            std::cout << "nota: " << get_media_avaliacao() << std::endl;
+        } else if (subtipo == "Serie") {
+            std::cout << "Série " << id << ": ";
+            std::cout << nome << " (" << ano << "), ";
+            std::cout << genero << ", " << n_temp << " temporadas, ";
+            std::cout << total_ep << " episódios, ";
+            std::cout << "nota: " << get_media_avaliacao() << std::endl;
+        }
+    }
 };
 
-#endif
+#endif // VIDEO_HPP
