f7a6c97f7c37c38513ba9080402763ff2b31a9d8
Geanderson4044 <129778207+Geanderson4044@users.noreply.github.com>
2023-06-04 16:13:48 -0300

    Update Streaming.hpp

--- Streaming.hpp
+++ Streaming.hpp
@@ -1,13 +1,15 @@
 #ifndef STREAMING_HPP
 #define STREAMING_HPP
-
 #include <vector>
 #include "Video.hpp"
 
 class Streaming {
+    
     private:
     std::vector<Video*> catalogo;
+    
 public:
+
     void cadastrar_filme(Video* v);
     void cadastrar_serie(Video* v);
     void avaliacao(int id, int nota);
