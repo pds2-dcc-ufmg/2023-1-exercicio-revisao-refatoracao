c847a6c3b5a689fa9926ef423a094892cc01b0ed
Geanderson4044 <129778207+Geanderson4044@users.noreply.github.com>
2023-06-04 16:14:23 -0300

    Update Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -1,6 +1,5 @@
 #ifndef VIDEO_HPP
 #define VIDEO_HPP
-
 #include <string>
 #include <list>
 
@@ -12,8 +11,8 @@ class Video {
     std::list<int> avaliacoes;
     
 public:
-    Video(std::string nome, int ano, std::string genero, std::string subtipo, int filme_duracao, int n_temp, int total_ep);
 
+    Video(std::string nome, int ano, std::string genero, std::string subtipo, int filme_duracao, int n_temp, int total_ep);
     float get_media_avaliacao();
     void avaliar(int nota);
     void print_info(int id);
