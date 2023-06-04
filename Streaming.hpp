b82edbc4b7db94b7ef587864aeebe1e4d95cb56a
Geanderson4044 <129778207+Geanderson4044@users.noreply.github.com>
2023-06-04 16:11:53 -0300

    Update Streaming.hpp

--- Streaming.hpp
+++ Streaming.hpp
@@ -5,14 +5,15 @@
 #include "Video.hpp"
 
 class Streaming {
+    private:
+    std::vector<Video*> catalogo;
 public:
     void cadastrar_filme(Video* v);
     void cadastrar_serie(Video* v);
     void avaliacao(int id, int nota);
     void print_catalogo();
 
-private:
-    std::vector<Video*> catalogo;
+
 };
 
 #endif
