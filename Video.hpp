1f066896bd296ef1d2521768fa3656bc31bb1f1c
PGazire <128267687+PGazire@users.noreply.github.com>
2023-06-05 21:32:36 -0300

    Melhorias Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -7,10 +7,10 @@
 
 class Video {
 private:
-int ano, filme_duracao, n_temp, total_ep;
+int ano, filme_duracao, n_temp, total_ep, nota, id;
 string nome, genero, subtipo;
 list<int> avaliacoes;
-    
+   
 public:
 
 Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep);
