1b562679625864ecbddaa4ed5005d0b550fbf721
kafbb <128070792+kafbb@users.noreply.github.com>
2023-06-06 20:56:27 -0300

    Create Video.cpp

---
+++ Video.cpp
@@ -0,0 +1,22 @@
+#ifndef VIDEO_HPP
+#define VIDEO_HPP
+
+#include <string>
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class Video {
+public:
+    string nome, genero, subtipo;
+    int ano, duracaoFilme, numTemporadas, totalEpisodios;
+    list<int> avaliacoes;
+
+    Video(string nome, int ano, string genero, string subtipo, int duracaoFilme, int numTemporadas, int totalEpisodios);
+    float calcularMediaAvaliacao();
+    void adicionarAvaliacao(int nota);
+    virtual void imprimirInfo(int id) = 0; // Método puramente virtual
+};
+
+#endif
