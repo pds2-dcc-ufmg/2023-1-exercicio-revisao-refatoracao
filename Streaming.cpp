5e54e0c28155937e710197f3a6f0ac88a8f0f1bc
Antônio Temponi Marques <antoniotemponi999@gmail.com>
2023-06-06 20:31:17 -0300

    Fix: Streaming.hpp e cpp

---
+++ Streaming.cpp
@@ -0,0 +1,27 @@
+#include "Streaming.hpp"
+#include <iostream>
+
+void Streaming::cadastrar_filme(Video* v) {
+    catalogo.push_back(v);
+}
+
+void Streaming::cadastrar_serie(Video* v) {
+    catalogo.push_back(v);
+}
+
+void Streaming::avaliacao(int id, int nota) {
+    catalogo[id - 1]->avaliar(nota);
+}
+
+void Streaming::print_catalogo() {
+    if (catalogo.size() == 0) {
+        std::cout << "Sem video cadastrado!" << std::endl;
+    } else {
+        std::cout << "Catalogo Streaming:" << std::endl;
+        int i = 0;
+        for (std::vector<Video*>::iterator it = catalogo.begin(); it != catalogo.end(); it++) {
+            (*it)->print_info(i + 1);
+            i++;
+        }
+    }
+}
