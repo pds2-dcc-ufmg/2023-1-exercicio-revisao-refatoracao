94b8acc607c1e09de013e52625aef699c269e7e7
Antônio Temponi Marques <antoniotemponi999@gmail.com>
2023-06-06 20:24:42 -0300

    Fix: main (formatar)

--- main.cpp
+++ main.cpp
@@ -7,54 +7,60 @@
 int main()
 
 {
-Streaming ss;
-string t;
+    Streaming ss;
+    string t;
 
-while (cin >> t) {
-if (t == "Filme")
+    while (cin >> t)
+    {
+        if (t == "Filme")
 
-{
-string n, g;
-int a, d;
-cin >> g >> a >> d;
-getline(cin, n);
+        {
+            string n, g;
+            int a, d;
+            cin >> g >> a >> d;
+            getline(cin, n);
 
-Video *v;
-v = new Video(n, a, g, t, d, 0, 0);
-if (50 <= d)
+            Video *v;
+            v = new Video(n, a, g, t, d, 0, 0);
+            if (50 <= d)
 
-{
-ss.cadastrar_filme(v);
-}
-}
-if (t == "Serie") {
+            {
+                ss.cadastrar_filme(v);
+            }
+        }
+        if (t == "Serie")
+        {
 
-string n;
-string g;
-int a, nt, te;
-cin >> g >> a >> nt >> te;
-getline(cin, n);
-Video *v;
-v = new Video(n, a, g, t, 0, nt, te);
-if (2 <= te) {
-{
-ss.cadastrar_serie(v);
-}
-}
-}
+            string n;
+            string g;
+            int a, nt, te;
+            cin >> g >> a >> nt >> te;
+            getline(cin, n);
+            Video *v;
+            v = new Video(n, a, g, t, 0, nt, te);
+            if (2 <= te)
+            {
+                {
+                    ss.cadastrar_serie(v);
+                }
+            }
+        }
 
-if (t == "Nota") {
-int i, n;
-cin >> i >> n;
+        if (t == "Nota")
+        {
+            int i, n;
+            cin >> i >> n;
 
-if (n <= 10) {
-if (n >= 0) {
-ss.avaliacao(i, n);
-}
-}
-}
-}
-ss.print_catalogo();
+            if (n <= 10)
+            {
+                if (n >= 0)
+                {
+                    ss.avaliacao(i, n);
+                }
+            }
+        }
+    }
+    ss.print_catalogo();
 
-return 0;
+    return 0;
 }
