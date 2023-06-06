948f898ed50dae901927afcbf81506aac7fb1bc0
PGazire <128267687+PGazire@users.noreply.github.com>
2023-06-06 00:53:00 -0300

    Melhoria Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -6,14 +6,17 @@
 #include <list>
 
 class Video {
+
 private:
-int ano, filme_duracao, n_temp, total_ep, nota, id;
-string nome, genero, subtipo;
-list<int> avaliacoes;
+std::string nome;
+int ano;
+std::string genero, subtipo;
+int filme_duracao, n_temp, total_ep, nota, id;
+std::list<int> avaliacoes;
    
 public:
 
-Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep);
+Video(std::string nome, int ano, std :: string genero, std::string subtipo, int filme_duracao, int n_temp, int total_ep);
 
 float get_media_avaliacao();
 
