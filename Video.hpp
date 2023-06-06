224dd5664d712298277cba8bbf291585cc7fbd43
kafbb <128070792+kafbb@users.noreply.github.com>
2023-06-06 20:57:40 -0300

    Update Video.hpp

--- Video.hpp
+++ Video.hpp
@@ -9,46 +9,15 @@ using namespace std;
 
 class Video {
 public:
-int ano, filme_duracao, n_temp, total_ep;
-string nome, genero, subtipo;
-list<int> avaliacoes;
+    string nome, genero, subtipo;
+    int ano, duracaoFilme, numTemporadas, totalEpisodios;
+    list<int> avaliacoes;
 
-Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep) : nome(nome), ano(ano), genero(genero), subtipo(subtipo), filme_duracao(filme_duracao),n_temp(n_temp), total_ep(total_ep)
-{
-}
-
-float get_media_avaliacao() {
-    float v = 0;
-    for (auto n : avaliacoes)v += n;
-    return v/avaliacoes.size();
-  }
-
-void avaliar(int nota) {
-  avaliacoes.push_back(nota);
-  }
-
-void print_info(int id){
-  if (subtipo == "Filme") 
-  {
-    cout << "Filme " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero;
-    cout << ", " << filme_duracao << " min, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-
-  if (subtipo == "Serie")
-  {
-    cout << "Serie " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero << ", ";
-    cout << n_temp << " temporadas, ";
-    cout << total_ep << " episodios, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-};
+    Video(string nome, int ano, string genero, string subtipo, int duracaoFilme, int numTemporadas, int totalEpisodios);
+    float calcularMediaAvaliacao();
+    void adicionarAvaliacao(int nota);
+    virtual void imprimirInfo(int id) = 0; // Método puramente virtual
 };
 
 #endif
+
