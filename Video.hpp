40b88868b4d8301a707dfc5befdb71df60f81be1
leomarql <leo.marques03@hotmail.com>
2023-06-06 22:17:23 -0300

    fixed: video.hpp

--- Video.hpp
+++ Video.hpp
@@ -8,47 +8,19 @@
 using namespace std;
 
 class Video {
-public:
-int ano, filme_duracao, n_temp, total_ep;
-string nome, genero, subtipo;
-list<int> avaliacoes;
+  private:
+    int _ano, _filme_duracao, _n_temp, _total_ep;
+    string _nome, _genero, _subtipo;
+    list<int> avaliacoes;
 
-Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep) : nome(nome), ano(ano), genero(genero), subtipo(subtipo), filme_duracao(filme_duracao),n_temp(n_temp), total_ep(total_ep)
-{
-}
+  public:
+    Video(string nome, int ano, string genero, string subtipo, int filme_duracao, int n_temp, int total_ep);
 
-float get_media_avaliacao() {
-    float v = 0;
-    for (auto n : avaliacoes)v += n;
-    return v/avaliacoes.size();
-  }
+    float get_media_avaliacao();
 
-void avaliar(int nota) {
-  avaliacoes.push_back(nota);
-  }
+    void avaliar(int nota);
 
-void print_info(int id){
-  if (subtipo == "Filme") 
-  {
-    cout << "Filme " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero;
-    cout << ", " << filme_duracao << " min, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-
-  if (subtipo == "Serie")
-  {
-    cout << "Serie " << id << ":";
-    cout << nome;
-    cout << " (" << ano << "), ";
-    cout << genero << ", ";
-    cout << n_temp << " temporadas, ";
-    cout << total_ep << " episodios, ";
-    cout << "nota: " << get_media_avaliacao() << std::endl;
-  }
-};
+    void print_info(int id);
 };
 
 #endif
